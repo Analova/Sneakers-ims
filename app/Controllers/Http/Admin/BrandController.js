8e65e9911c6443bf7642ab3ddd52a7c52e088915
Ana Nikolova <ani.nik19@gmail.com>
2020-04-16 11:54:02 +0200

    show al brands

--- app/Controllers/Http/Admin/BrandController.js
+++ app/Controllers/Http/Admin/BrandController.js
@@ -5,22 +5,18 @@ const sanitize = require("sqlstring");
 class BrandController {
   async index({ view, request, response }) {
     try {
-      let allProducts = await Database.raw(`
-        SELECT products.id,
-        products.title, products.sku, brands.title as brand,
-        concat(users.f_name, ' ', users.l_name) as user,
-        products.material, products.qty, products.size,
-        products.user_id, products.created_at
-        FROM products
-        INNER JOIN brands
-        ON products.brand_id = brands.id
+      let allBrands = await Database.raw(`
+        SELECT brands.id, brands.title, brands.img_url, brands.user_id,
+        concat(users.f_name, " " ,users.l_name ) as user,
+        brands.created_at, brands.updated_at
+        FROM brands
         INNER JOIN users
-        ON products.user_id = users.id
+        ON brands.user_id = users.id
         ORDER BY created_at ASC
       `);
-      allProducts = allProducts[0];
+      allBrands = allBrands[0];
 
-      return view.render("admin/products/all", { allProducts });
+      return view.render("admin/brands/all", { allBrands });
     } catch (error) {
       console.log(error);
       return response.redirect("back");
